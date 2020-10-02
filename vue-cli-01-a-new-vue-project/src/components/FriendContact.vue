23dc84c0025493795440a3bde65381df054028fe
jeanromain KRUPA <jkrupa@hotmail.fr>
2020-10-02 15:08:38 +0200

    done

---
+++ vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,31 @@
+<template>
+    <li>
+        <h2>{{ friend.name }}</h2>
+        <button @click="toggleDetail">{{detailsAreVisible ? 'Hide' : 'Show'}}  Details</button>
+        <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong>{{ friend.phone }}</li>
+            <li><strong>Email:</strong>{{ friend.email }} </li>
+        </ul>
+    </li>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            detailsAreVisible: false,
+            friend: {
+                id: 'manuel',
+                name: 'manuel loren',
+                phone: '090909099089',
+                email: 'jofeo@ioou.com'
+            }
+        }
+    },
+    methods: {
+        toggleDetail() {
+            this.detailsAreVisible = !this.detailsAreVisible
+        }
+    }
+}
+</script>
