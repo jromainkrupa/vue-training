f60eba4c0412667c075fc3a8dae4ac92f9ad93e8
jeanromain KRUPA <jkrupa@hotmail.fr>
2020-10-02 14:47:57 +0200

    exercice done

---
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <h2>My friends</h2>
+    <ul>
+      <li></li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: 'manuel',
+          name: 'manuel loren',
+          phone: '090909099089',
+          email: 'jofeo@ioou.com'
+        },
+        {
+          id: 'julie',
+          name: 'julie jon',
+          phone: '090909089',
+          email: 'jofeo@ioou.com'
+        }
+      ]
+    }
+  }
+}
+</script>
