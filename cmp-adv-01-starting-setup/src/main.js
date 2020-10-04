8958612b03a7fddc12ef1fc22fdbf46d6453940d
jeanromain KRUPA <jkrupa@hotmail.fr>
2020-10-04 15:43:21 +0200

    slot done

--- cmp-adv-01-starting-setup/src/main.js
+++ cmp-adv-01-starting-setup/src/main.js
@@ -2,11 +2,11 @@ import { createApp } from 'vue';
 
 import App from './App.vue';
 import BaseBadge from './components/BaseBadge.vue';
-import BadgeList from './components/BadgeList.vue';
-
+import BaseCard from './components/BaseCard.vue';
+import BadgeList from './components/BadgeList'
 const app = createApp(App);
 
 app.component('base-badge', BaseBadge);
 app.component('badge-list', BadgeList);
-
+app.component('base-card', BaseCard);
 app.mount('#app');
