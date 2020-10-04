da23750b011c42d97a9e2e7d5593ad3c95c509cd
jeanromain KRUPA <jkrupa@hotmail.fr>
2020-10-04 16:21:54 +0200

    scoped slots

---
+++ cmp-adv-01-starting-setup/src/components/CourseGoals.vue
@@ -0,0 +1,16 @@
+<template>
+    <ul>
+        <li v-for="goal in goals" :key="goal">
+            <slot :item="goal" antoher-pro="..."></slot>
+        </li>
+    </ul>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            goals: ['finish the course', 'Learn Vue'],
+        };
+    }
+}
+</script>
