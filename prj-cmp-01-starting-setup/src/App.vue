08b3a1a5db3f88fa32c54be4a7bc102ebf94937e
jeanromain KRUPA <jkrupa@hotmail.fr>
2020-10-05 16:30:37 +0200

    props project

---
+++ prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+    <ul>
+        <learning-ressource 
+        v-for="storedRessource in storedRessources" 
+        :key="storedRessource.id"
+        :title="storedRessource.title"
+        :description="storedRessource.description"
+        :link="storedRessource.link"></learning-ressource>
+    </ul>
+</template>
+
+<script>
+import LearningRessource from './components/learning-ressources/LearningRessource.vue'
+export default {
+    components: {
+        LearningRessource
+    },
+    data() {
+        return {
+            storedRessources: [
+                {
+                    id: 'official guide',
+                    title: 'Official Guide',
+                    description: 'The official vue js documentation',
+                    link: 'http://vuejs.org'
+                },
+                {
+                    id: 'google',
+                    title: 'google',
+                    description: 'so cool google',
+                    link: 'http://google.org'
+                }
+            ]
+        }
+    }
+}
+</script>
