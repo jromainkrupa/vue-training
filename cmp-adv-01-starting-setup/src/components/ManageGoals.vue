d1433fc690d5e72e392e702346a6b4223231505b
jeanromain KRUPA <jkrupa@hotmail.fr>
2020-10-05 11:05:23 +0200

    dialog tag and slot

--- cmp-adv-01-starting-setup/src/components/ManageGoals.vue
+++ cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -1,3 +1,36 @@
 <template>
-    <h2>Manage Goals</h2>
+    <div>
+        <h2>Manage Goals</h2>
+        <input type="text" ref="goal">
+        <button @click="setGoal">Set Goal</button>
+        <error-alert v-if="inputIsInvalid">
+            <h2>Input Is Invalid</h2>
+            <p>Please enter somthing</p>
+            <button @click="confirmError">OK</button>
+        </error-alert>
+    </div>
 </template>
+<script>
+import ErrorAlert from './ErrorAlert.vue'
+export default {
+    components: {
+        ErrorAlert
+    },
+    data() {
+        return {
+            inputIsInvalid: false
+        }
+    },
+    methods: {
+        setGoal() {
+            const enteredValue = this.$refs.goal.value
+            if (enteredValue === '') {
+                this.inputIsInvalid = true
+            }
+        },
+        confirmError() {
+            this.inputIsInvalid = false
+        }
+    }
+}
+</script>
