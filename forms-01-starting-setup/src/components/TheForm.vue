159994fa5122b618a3dfae9b81df6b12b488513a
jeanromain KRUPA <jkrupa@hotmail.fr>
2020-10-07 14:33:33 +0200

    form control

--- forms-01-starting-setup/src/components/TheForm.vue
+++ forms-01-starting-setup/src/components/TheForm.vue
@@ -1,8 +1,9 @@
 <template>
   <form @submit.prevent="submitForm">
-    <div class="form-control">
+    <div class="form-control" :class="{invalid : userNameValidity === 'invalid'}">
       <label for="user-name">Your Name</label>
-      <input id="user-name" name="user-name" type="text" v-model="userName" />
+      <input id="user-name" name="user-name" type="text" v-model="userName" @blur="validateInput" />
+      <p v-if="userNameValidity === 'invalid'">Please enter a valid name</p>
     </div>
     <div class="form-control">
       <label for="age">Your Age (Years)</label>
@@ -64,7 +65,8 @@ export default {
       referrer: 'wom',
       interest: [],
       how: null,
-      terms: false
+      terms: false,
+      userNameValidity: 'pending'
     }
   },
   methods: {
@@ -75,6 +77,13 @@ export default {
       this.interest = [];
       this.how = null;
       this.confirm = false;
+    },
+    validateInput() {
+      if (this.userName.trim() === '') {
+        this.userNameValidity = 'invalid'
+      } else {
+        this.userNameValidity = "valid"
+      }
     }
   }
 }
@@ -92,6 +101,12 @@ form {
 
 .form-control {
   margin: 0.5rem 0;
+}
+.form-control.invalid input {
+  border-color: red;
+}
+.form-control.invalid label {
+  color: red;
 }
 
 label {
