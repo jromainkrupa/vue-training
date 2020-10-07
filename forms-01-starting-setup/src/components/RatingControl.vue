d598776b422424ad65b6bc59934000bc806f0d0b
jeanromain KRUPA <jkrupa@hotmail.fr>
2020-10-07 15:06:44 +0200

    form control

---
+++ forms-01-starting-setup/src/components/RatingControl.vue
@@ -0,0 +1,50 @@
+<template>
+    <ul>
+        <li><button type="button" @click="activate('poor')">Poor</button></li>
+        <li><button type="button" @click="activate('average')">Average</button></li>
+        <li><button type="button" @click="activate('great')">Great</button></li>
+    </ul>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            activeOption: null
+        }
+    },
+    methods: {
+        activate(option) {
+            this.activeOption = option
+        }
+    }
+}
+</script>
+
+<style scoped>
+.active {
+    border-color: rgb(59, 20, 59);
+}
+.active button {
+    color: rgb(59, 20, 59)
+}
+ul {
+    list-style: none;
+    margin: 0.5rem 0;
+    padding:0;
+    display: flex;
+}
+li {
+    margin: 0 1rem;
+    border: 1px solid #ccc;
+    padding: 1rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+button {
+    font: inherit;
+    border: none;
+    background-color: transparent;
+    cursor: pointer;
+}
+</style>
--- forms-01-starting-setup/src/components/TheForm.vue
+++ forms-01-starting-setup/src/components/TheForm.vue
@@ -48,6 +48,9 @@
       </div>
     </div>
     <div class="form-control">
+      <rating-control></rating-control>
+    </div>
+    <div class="form-control">
       <input type="checkbox" id="confirm-terms" name="confirm-terms" v-model="confirm">
       <label for="confirm-terms">Agree to terms of use ?</label>
     </div>
@@ -57,7 +60,12 @@
   </form>
 </template>
 <script>
+import RatingControl from './RatingControl.vue'
+
 export default {
+  components: {
+    RatingControl
+  },
   data() {
     return {
       userName:'',
@@ -77,6 +85,7 @@ export default {
       this.interest = [];
       this.how = null;
       this.confirm = false;
+      console.log('confirm')
     },
     validateInput() {
       if (this.userName.trim() === '') {
