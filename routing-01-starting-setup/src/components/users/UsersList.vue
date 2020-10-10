022ec6c2662245a49c80cf8a3694e62c91860ada
jeanromain KRUPA <jkrupa@hotmail.fr>
2020-10-10 16:18:29 +0200

    before route leave

--- routing-01-starting-setup/src/components/users/UsersList.vue
+++ routing-01-starting-setup/src/components/users/UsersList.vue
@@ -1,5 +1,6 @@
 <template>
 <button @click="confirmInput">Confirm</button>
+<button @click="saveChanges">save changes</button>
   <ul>
     <user-item v-for="user in users" :key="user.id" :name="user.fullName" :role="user.role"></user-item>
   </ul>
@@ -13,17 +14,39 @@ export default {
     UserItem,
   },
   inject: ['users'],
+  data() {
+    return {
+      changesSaved: false,
+    }
+  },
   methods: {
     confirmInput() {
       //do somehting
       this.$router.push('/teams')
       // many methods thanks to the router this.$router.forward()
+    },
+    saveChanges() {
+      this.changesSaved = true
     }
   },
   beforeRouteEnter(to, from, next) {
     console.log('before route enter inside component')
     console.log(to, from)
     next()
+  },
+  beforeRouteLeave(to, from, next) { // useful for unsave form for accidentaly leaving a page !
+    console.log('users list cmp before route leave')
+    console.log(to, from)
+    if (this.changesSaved === true) {
+      next()
+    } else {
+      const userwantsToLeave = confirm('are you sure ? you got unsaved changes');
+      next(userwantsToLeave)
+    }
+
+  },
+  unmounted() {
+    console.log('unmounted') 
   }
 };
 </script>
--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -53,10 +53,10 @@ router.beforeEach(function(to, from, next) {
     next() // pass tru, false or string (route) or object with name: 'team-mebers', params { teamId: 't2}
 }); // every time you navigate from a page to another
 
-router.afterEach(to, from) {
+router.afterEach(function(to, from){ 
     console.log(to, from) // sending analytics data for example 
     // not useful to authentication or user view control
-}
+})
 
 const app = createApp(App)
 
