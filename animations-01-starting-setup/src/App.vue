de5abe3ff84283c4bd57472ed3fc5d8ac4b34ef3
jeanromain KRUPA <jkrupa@hotmail.fr>
2020-10-11 09:57:49 +0200

    transition component with v-enter...

--- animations-01-starting-setup/src/App.vue
+++ animations-01-starting-setup/src/App.vue
@@ -3,6 +3,12 @@
     <div class="block" :class="{animate: animatedBlock}"></div>
     <button @click="animateBlock">Animate</button>
   </div>
+  <div class="container" >
+    <transition>
+      <p v-if="paraIsVisible">This is only sometimes visible</p>
+    </transition>
+    <button @click="toggleParagraph">Toggle paragraph</button>
+  </div>
   <base-modal @close="hideDialog" v-if="dialogIsVisible">
     <p>This is a test dialog!</p>
     <button @click="hideDialog">Close it!</button>
@@ -17,10 +23,14 @@ export default {
   data() {
     return { 
       dialogIsVisible: false,
-      animatedBlock: false
+      animatedBlock: false,
+      paraIsVisible: false
     };
   },
   methods: {
+    toggleParagraph() {
+      this.paraIsVisible = !this.paraIsVisible
+    },
     showDialog() {
       this.dialogIsVisible = true;
     },
@@ -79,6 +89,32 @@ button:active {
 .animate {
   /* transform: translateX(-150px); */
   animation: slide-fade 0.3s ease-out forwards ;
+}
+.v-enter-from {
+  opacity: 0;
+  transform: translateY(-30px);
+}
+
+.v-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.v-enter-to {
+  opacity: 1;
+  transform: translateY(0);
+}
+.v-leave-from {
+  opacity: 1;
+  transform: translateY(0);
+}
+
+.v-leave-to {
+  opacity: 0;
+  transform: translateY(+30px);
+}
+
+.v-leave-active {
+  transition: all 0.3s ease-out;
 }
 
 @keyframes slide-fade { /* name up to us*/
