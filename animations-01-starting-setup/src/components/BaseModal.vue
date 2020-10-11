0bde5f315353fc0a4b095a782e9e45db1d38f28f
jeanromain KRUPA <jkrupa@hotmail.fr>
2020-10-11 10:49:44 +0200

    animation

--- animations-01-starting-setup/src/components/BaseModal.vue
+++ animations-01-starting-setup/src/components/BaseModal.vue
@@ -1,12 +1,15 @@
 <template>
-  <div class="backdrop" @click="$emit('close')"></div>
-  <dialog open>
-    <slot></slot>
-  </dialog>
+  <div v-if="open" class="backdrop" @click="$emit('close')"></div>
+  <transition name="modal">
+    <dialog open v-if="open">
+      <slot></slot>
+    </dialog>
+  </transition>
 </template>
 
 <script>
 export default {
+  props: ['open'],
   emits: ['close'],
 };
 </script>
@@ -34,12 +37,19 @@ dialog {
   background-color: white;
   z-index: 100;
   border: none;
-  animation: modal 0.5s ease-out forwards;
+  /* animation: modal 0.5s ease-out forwards; */
 }
+.modal-enter-active {
+  animation: modal 0.5s ease-out;
+}
+.modal-leave-active {
+  animation: modal 0.5s ease-in reverse;
+}
+
 @keyframes modal {
   from {
     opacity: 0;
-    transform: translateX(-50px) scale(0.8);
+    transform: translateX(-150px) scale(0.2);
   }
   to {
     opacity: 1;
