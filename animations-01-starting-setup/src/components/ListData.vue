b2681aa29eb847a5ebbd0a06ef6ed79d0a47a5d6
jeanromain KRUPA <jkrupa@hotmail.fr>
2020-10-12 14:50:10 +0200

    transition group

---
+++ animations-01-starting-setup/src/components/ListData.vue
@@ -0,0 +1,63 @@
+<template>
+    <transition-group tag="ul" name="list-data">
+        <li v-for="user in users" :key="user" @click="removeUser(user)">{{ user }} </li>
+    </transition-group>
+    <div>
+        <input type="text" ref="userNameInput">
+        <button @click="addUser">Add User</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            users: ['Max', 'Manue', 'Julie', 'Angela', 'Mickael'],
+        }
+    },
+    methods: {
+        removeUser(user) {
+            this.users = this.users.filter(usr => usr !== user);
+        },
+        addUser() {
+            const enteredUsername = this.$refs.userNameInput.value
+            this.users.unshift(enteredUsername)
+        }
+    }
+}
+</script>
+<style scoped>
+ul {
+    list-style: none;
+    margin: 1rem 0;
+    padding: 0;
+}
+
+li {
+    border: 1px solid #cccccc;
+    padding: 1rem;
+    text-align: center;
+}
+.list-data-enter-from {
+    opacity: 0;
+    transform: translateX(-30px);
+}
+.list-data-enter-active {
+    transition: all 1s ease-out
+}
+.list-data-enter-to {
+    opacity: 1;
+    transform: translateX(0);
+}
+.list-data-leave-from {
+    opacity: 1;
+    transform: translateX(0px);
+}
+.list-data-leave-active {
+    transition: all 1s ease-in
+}
+.list-data-leave-to {
+    opacity: 1;
+    transform: translateX(30px);
+}
+</style>
